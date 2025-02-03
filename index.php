e0ecc5d8b1dd085c933121ed56123b102faf6f25
sirokyo <ondrej.siroky8@gmail.com>
2025-02-03 20:47:58 +0100

    fixed problem with printing reservations

--- index.php
+++ index.php
@@ -1,52 +1,3 @@
-<!DOCTYPE html>
-<html lang="cs">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="styles.css">
-    <title>Rezervace místností</title>
-</head>
-<body>
-   <h1>Rezervace místností</h1> <br>
-   
-   <form method="post">
-        <h2>Vytvoření rezervace</h2>
-
-        <!-- Input pro jméno a příjmení -->
-        <label for="jmeno">Jméno a příjmení:</label> <br>
-        <input type="text" name="jmeno" id="jmeno" placeholder="Jan Novák" required> <br>
-
-        <!-- Vybrání místnosti -->
-        <label for="mistnost">Vyberte si místnost:</label> <br>
-        <select name="mistnost" id="vybrat-mistnost" required>
-            <option value="Místnost 1">Místnost 1</option>
-            <option value="Místnost 2">Místnost 2</option>
-            <option value="Místnost 3">Místnost 3</option>
-            <option value="Místnost 4">Místnost 4</option>
-        </select> <br>
-
-        <!-- Vybrání dne -->
-        <label for="den">Vyberte den:</label> <br>
-        <input type="date" name="den" required> <br>
-
-        <!-- Vybrání času -->
-        <label for="zacatek">Čas od:</label>
-        <input type="time" name="zacatek" required> <br>
-        <label for="konec">Čas do:</label>
-        <input type="time" name="konec" required> <br> 
-        
-        <!-- Odeslání formuláře -->
-        <input type="submit" value="Rezervovat" name="odeslat">
-   </form>
-   <aside>
-        <h2>Vytvořené rezervace</h2>
-        <?php
-            vypsat();
-        ?>
-   </aside>
-</body>
-</html>
-
 <?php
     # Funkce pro zápis dat do souboru
     function zapsat($jmeno, $mistnost, $den, $zacatek, $konec) {
@@ -141,3 +92,50 @@
         zapsat($jmeno, $mistnost, $den, $zacatek, $konec);
     }
 ?>
+
+<!DOCTYPE html>
+<html lang="cs">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <title>Rezervace místností</title>
+</head>
+<body>
+   <h1>Rezervace místností</h1> <br>
+   
+   <form method="post">
+        <h2>Vytvoření rezervace</h2>
+
+        <!-- Input pro jméno a příjmení -->
+        <label for="jmeno">Jméno a příjmení:</label> <br>
+        <input type="text" name="jmeno" id="jmeno" placeholder="Jan Novák" required> <br>
+
+        <!-- Vybrání místnosti -->
+        <label for="mistnost">Vyberte si místnost:</label> <br>
+        <select name="mistnost" id="vybrat-mistnost" required>
+            <option value="Místnost 1">Místnost 1</option>
+            <option value="Místnost 2">Místnost 2</option>
+            <option value="Místnost 3">Místnost 3</option>
+            <option value="Místnost 4">Místnost 4</option>
+        </select> <br>
+
+        <!-- Vybrání dne -->
+        <label for="den">Vyberte den:</label> <br>
+        <input type="date" name="den" required> <br>
+
+        <!-- Vybrání času -->
+        <label for="zacatek">Čas od:</label>
+        <input type="time" name="zacatek" required> <br>
+        <label for="konec">Čas do:</label>
+        <input type="time" name="konec" required> <br> 
+        
+        <!-- Odeslání formuláře -->
+        <input type="submit" value="Rezervovat" name="odeslat">
+   </form>
+   <aside>
+        <h2>Vytvořené rezervace</h2>
+        <?php vypsat(); ?>
+   </aside>
+</body>
+</html>
